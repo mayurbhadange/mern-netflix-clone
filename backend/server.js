68c6a8ccc6cdc714035ae106b8697ca6a1be8970
mayurbhadange <mayurbhadange2004@gmail.com>
2025-03-26 12:19:11 +0530

    initial-commit

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import path from 'path';
 
@@ -11,55 +12,31 @@ import { ENV_VARS } from './config/envVars.js';
 import { connectDB } from './config/db.js';
 import { protectRoute } from './middleware/protectRoute.js';
 
-
-
-
 const app = express();
-const PORT = ENV_VARS.PORT
+const PORT = ENV_VARS.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json()); //allow us to parse req.body 
-app.use(cookieParser()); //allow us to parse cookies
+// CORS configuration
+app.use(
+  cors({
+    origin: 'https://netflix-clone-mu-one-36.vercel.app',
+    credentials: true, // Allow cookies and authentication headers
+  })
+);
 
-app.use("/api/v1/auth",   authRoutes);
-app.use("/api/v1/movie", protectRoute,movieRoutes);
-app.use("/api/v1/tv", protectRoute,tvRoutes);
-app.use("/api/v1/search", protectRoute,searchRoutes);
+app.use(express.json()); // Allow parsing req.body
+app.use(cookieParser()); // Allow parsing cookies
 
-// ENV_VARS.NODE_ENV === 'production'
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/movie', protectRoute, movieRoutes);
+app.use('/api/v1/tv', protectRoute, tvRoutes);
+app.use('/api/v1/search', protectRoute, searchRoutes);
 
-
-app.get("/", (req, res) => {
-  res.send("Server is running...");
+app.get('/', (req, res) => {
+  res.send('Server is running...');
 });
-
 
 app.listen(PORT, () => {
-    console.log('Server started at http://localhost:'+PORT);
-    connectDB();
+  console.log('Server started at http://localhost:' + PORT);
+  connectDB();
 });
-
-// const options = { 
-//     method: 'GET',
-//     headers: {
-//       accept: 'application/json',
-//       Authorization: 'Bearer ' + ENV_VARS.TMDB_API_KEY
-//     }
-//   };
-  
-  // fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
-  //   .then(res => res.json())
-  //   .then(res => console.log(res))
-  //   .catch(err => console.error(err));
-
-  // fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
-  // .then(res => {
-  //   if (!res.ok) throw new Error(`HTTP error! Status: ${res.status}`);
-  //   return res.json();
-  // })
-  // .then(res => console.log(res))
-  // .catch(err => console.error('Fetch Error:', err));
-
-
-
-    
